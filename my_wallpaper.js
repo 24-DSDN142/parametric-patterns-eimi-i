0ba6dedf23798b5446c025b47a9463e90960bb7f
eimi-i <169110475+eimi-i@users.noreply.github.com>
2024-07-29 22:19:45 +1200

    Update my_wallpaper.js
    jellyfish

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -3,18 +3,18 @@ let rect_width  = 20;
 let rect_height = 20;
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 100;
 }
 
 function wallpaper_background() {
-  background(63, 71, 104); //light blue colour
+  background(63, 71, 104); //dark muted blue colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
@@ -23,47 +23,63 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
   //Jellyfish
   push();
-  // translate(-30,25)
-  // rotate(-25)
+  translate(-30,25)
+  rotate(-25)
 
+  //BLUE JELLYFISH
   //head
-
-  fill(183,203,246);
+  fill(183,203,246); 
   stroke(183,203,246);
-  arc(55, 55, 50, 75, 180,0);
-  ellipse()
-
-
-  fill(180,107,192);
-  stroke(180,107,192);
-  beginShape();
-  vertex(130,110);
-  quadraticVertex(140,10,150,110);
+  arc(55, 55, 50, 65, 180,0); //semicircle top of head
+  beginShape(); //bottom part of head
+  vertex(55,55);
+  bezierVertex();
+  bezierVertex();
   endShape(CLOSE);
+  
+  //legs
+  noFill();
+  stroke(183,203,246);
+  strokeWeight(0.7);
+  bezier(40,55,15,65,80,90,40,120)
+  bezier(70,55,100,65,40,90,80,120)
+  stroke(180,107,192)
+  bezier(50,55,30,70,90,100,30,110)
+  bezier(65,55,80,70,50,100,90,112)
+
+
+  fill(183,203,246); //blue jellyfish
+  stroke(183,203,246);
+  ellipse(55,63,10,20);
+  ellipse(55,84,8,18);
+  ellipse(55,104,4,14);
+  pop();
+
+  //PURPLE JELLYFISH
+  push();
+  // translate(330,140);
+  // rotate(140);
+  //head
+  fill(180,107,192); //purple jellyfish
+  stroke(180,107,192);
+  
 
   //legs
- 
-  fill(183,203,246);
-  stroke(183,203,246);
-  ellipse(55,63,10,20)
-  ellipse(55,85,8,18)
-  ellipse(55,106,6,16)
-  ellipse(55,125,4,14)
-  pop()
+
+
+  pop();
 
   //bubbles
-
   fill(255,220,255);
   stroke(240,220,255);
   ellipse(90,20,3.5,3.5); //small bubble to the top
   ellipse(140,35,17,17); //big bubble up top
   ellipse(40,155,20,20); //big bubble down bottom LEFT
   ellipse(14,130,3,3); //small bubble BOTTOM LEFT
-  Nofill();
 
   //shines
 
-  fill(183,203,246);
+ 
 
   //highlights D:
 
