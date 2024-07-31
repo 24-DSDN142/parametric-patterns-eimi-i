248b639748cf9187d1f32d34dfd79617826d02bf
eimi-i <169110475+eimi-i@users.noreply.github.com>
2024-08-01 10:29:30 +1200

    var
    more vars and rearrange jellyfish code order developing else

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,20 +1,16 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
-
-var baseColour = [183,203,246]; // colour for jellyfish and bubble 1
-var bubbleColour = [245, 203, 245]; // pink colour for highlights and bubble 2
-var shineColour = [219, 230, 255]; //colour of snines and inside of jellyfish
-var hX = 55; //jellyfish x axis
-var hY = 55; //jellyfish y axis
-var bubbleSize = 20
-var shineSize
-var tentacleWidth = 2
-
-
-
-
-//jellyfish rotation
+let baseColour = [183,203,246]; // colour for jellyfish and bubble 1
+let bubbleColour = [245, 203, 245]; // colour for highlights and bubble 2
+let shineColour = [219, 230, 255]; //colour of shines and inside of jellyfish
+let backgroundColour = [63, 71, 104];
+let hX = 45; //jellyfish x axis
+let hY = 40; //jellyfish y axis
+let hx = 45
+let hy = 60
+let bubbleSize = 20 //bubble size for both bubble types 
+let tentacleWidth = 3
+let LjellyfishAngle = -35
+let RjellyfishAngle = 40
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GRID_WALLPAPER);
@@ -22,29 +18,28 @@ function setup_wallpaper(pWallpaper) {
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
-  pWallpaper.grid_settings.cell_width  = 200;
-  pWallpaper.grid_settings.cell_height = 200;
+  pWallpaper.grid_settings.cell_width  = 190;
+  pWallpaper.grid_settings.cell_height = 190;
   pWallpaper.grid_settings.row_offset  = 100;
 }
 
 function wallpaper_background() {
-  background(63, 71, 104); //dark muted blue colour
+  background(backgroundColour); //dark muted blue colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-
 //LEFT JELLYFISH
   push();
   translate(-30,25)
-  rotate(-29)
+  rotate(LjellyfishAngle)
   DrawJellyfish(hX,hY);
   pop();
   
 //RIGHT JELLYFISH
   push();
-  translate(120,45);
-  rotate(30);
-  DrawJellyfish(hX,hY);
+  translate(140,35);
+  rotate(RjellyfishAngle);
+  DrawJellyfish(hx,hy);
   pop();
 
 //background
@@ -67,19 +62,16 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
    ellipse(50,165,bubbleSize,bubbleSize); //bottom left
    ellipse(116,37,bubbleSize,bubbleSize); //top middle
    ellipse(165,47,bubbleSize,bubbleSize); //top right
-   }
-  else {
-   fill(baseColour); //blue bubbles
-   stroke(baseColour);
-   ellipse(140,19,bubbleSize - 8, bubbleSize - 8); //big bubble up top
-   ellipse(30,155,17,17); //big bubble down bottom LEFT
-   ellipse(75,183,bubbleSize - 15,bubbleSize - 15);
-   ellipse(90,20,3.5,3.5); //small bubble to the top
-   ellipse(14,130,3,3); //small bubble BOTTOM LEFT
-
+   } else if(bubbleSize <= 19){
+    fill(baseColour); //blue bubbles
+    stroke(baseColour);
+    ellipse(140,19,bubbleSize - 8, bubbleSize - 8); //big bubble up top
+    ellipse(30,155,17,17); //big bubble down bottom LEFT
+    ellipse(75,183,bubbleSize - 15,bubbleSize - 15);
+    ellipse(90,20,3.5,3.5); //small bubble to the top
+    ellipse(14,130,3,3); //small bubble BOTTOM LEFT
    }
   
-
 }
 
 function DrawJellyfish(headX,headY) {
@@ -123,25 +115,27 @@ vertex(headX + 5,headY - 22);
 quadraticVertex(headX + 25,headY - 15,headX + 21,headY - 5);
 quadraticVertex(headX + 15,headY - 15,headX + 5,headY - 22);
 endShape(CLOSE);
+//legs
+noFill();
+stroke(baseColour); //base colour of jellyfish middle 2 legs
+strokeWeight(tentacleWidth); 
+bezier(headX - 15, headY, headX - 40, headY + 10, headX + 25, headY + 35, headX - 15, headY + 65);
+bezier(headX + 15, headY, headX + 45, headY + 10, headX - 15, headY + 35, headX + 25, headY + 65);
+stroke(bubbleColour); //brightest colour inner 2 legs
+bezier(headX - 5, headY + 1.4, headX - 25, headY + 15, headX + 35, headY + 45, headX - 25, headY + 55);
+bezier(headX + 10, headY, headX + 25, headY + 15, headX - 5, headY + 45, headX + 35, headY + 57);
+stroke(shineColour); //lightest colour outer 2 legs
+bezier(headX + 20, headY, headX + 55, headY + 25, headX - 15, headY + 35, headX + 5, headY + 65);
+bezier(headX - 22, headY + 4, headX - 55, headY + 15, headX + 15, headY + 35, headX - 29, headY + 65);
 //bottom rim
 fill(baseColour); //blue
 stroke(baseColour);
+strokeWeight(1);
 ellipse(headX - 25, headY,10,8);
 ellipse(headX - 15, headY - 2,10,8);
 ellipse(headX - 5, headY - 3,10,8);
 ellipse(headX + 5, headY - 3,10,8);
 ellipse(headX + 15, headY - 2,10,8);
 ellipse(headX + 25, headY - 2,10,8);
-//legs
-noFill();
-stroke(baseColour); //blue
-strokeWeight(tentacleWidth); 
-bezier(headX - 15, headY, headX - 40, headY + 10, headX + 25, headY + 35, headX - 15, headY + 65);
-bezier(headX + 15, headY, headX + 45, headY + 10, headX - 15, headY + 35, headX + 25, headY + 65);
-stroke(bubbleColour); //pink
-bezier(headX - 5, headY + 1.4, headX - 25, headY + 15, headX + 35, headY + 45, headX - 25, headY + 55);
-bezier(headX + 10, headY, headX + 25, headY + 15, headX - 5, headY + 45, headX + 35, headY + 57);
-stroke(shineColour); //light blue
-bezier(headX + 20, headY, headX + 55, headY + 25, headX - 15, headY + 35, headX + 5, headY + 65);
-bezier(headX - 22, headY + 4, headX - 55, headY + 15, headX + 15, headY + 35, headX - 29, headY + 65);
+
 }
